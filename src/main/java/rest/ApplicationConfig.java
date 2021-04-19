0ad8f78da7ab558803a2d2a23329daac4a453b54
Benjamin S <ben.sommer.skovgaard@gmail.com>
2021-04-19 09:10:12 +0200

    minor

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -22,11 +22,11 @@ public class ApplicationConfig extends Application {
     private void addRestResourceClasses(Set<Class<?>> resources) {
         resources.add(cors.CorsFilter.class);
         resources.add(errorhandling.AlreadyExistsMapper.class);
-        resources.add(errorhandling.GenericExceptionMappe
+        resources.add(errorhandling.GenericExceptionMapper.class);
         resources.add(errorhandling.InputNotValidMapper.class);
         resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
         resources.add(rest.CategoryResource.class);
-        resources.add(rest.UserResource.class)
+        resources.add(rest.UserResource.class);
         resources.add(security.JWTAuthenticationFilter.class);
         resources.add(security.LoginEndpoint.class);
         resources.add(security.RolesAllowedFilter.class);
